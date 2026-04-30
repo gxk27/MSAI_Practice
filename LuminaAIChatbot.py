b5f5e1fb7d597729715c46ba1dcf2ca27cb644ba
Noah Quilantan <100238310+gxk27@users.noreply.github.com>
2026-04-30 02:52:18 -0500

    Add files via upload

---
+++ LuminaAIChatbot.py
@@ -0,0 +1,93 @@
+
+#from the toolboxes, it is importing a tool to help with the ML code
+from sklearn.feature_extraction.text import CountVectorizer#turns words into numbers
+from sklearn.naive_bayes import MultinomialNB#the brain and learns from those numbers
+#imports operating system so it can talk to the code and vise versa
+import os
+from groq import Groq
+
+#creating spacers so output is more organized
+def spacers():
+    for _ in range(15):
+        print("* ", end="")
+    print("")
+    
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
+def ask_groq(user_input, intent): 
+    reply = client.chat.completions.create(
+    model= "llama-3.3-70b-versatile",
+    messages= [{"role": "system", "content": f"You are a friendly chatbot. The user's intent is {intent}\n"},
+              {"role": "user", "content": f"{user_input}"}
+         ]
+         
+    )
+    return (reply.choices[0].message.content)
+
+
+#list of tuples with types of 'greeting', 'help', and 'goodbye'
+sayings = [
+    ("hello", "greeting"),
+    ("yo", "greeting"),
+    ("whats up", "greeting"),
+    ("whats your name", "greeting"),
+    ("whats good", "greeting"),
+    ("hey how are you", "greeting"),
+    ("top of the morning", "greeting"),
+    ("good afternoon", "greeting"),
+    ("good evening chap", "greeting"),
+    ("help me", "help"),
+    ("can you assist me", "help"),
+    ("i need assistance", "help"),
+    ("are you able to help", "help"),
+    ("can you help me out", "help"),
+    ("assist me with this", "help"),
+    ("im heading out", "goodbye"),
+    ("i could use your brains for this", "help"),
+    ("see you later", "goodbye"),
+    ("adios", "goodbye"),
+    ("im out of here", "goodbye"),
+    ("bye", "goodbye"),
+    ("smell ya later", "goodbye"),
+    ("ill see you when i see you", "goodbye")
+]
+
+#greating labels with 0 being the phrase"x" and 1 being the type of phrase "1"
+# separate the phrases and labels
+X = [saying[0] for saying in sayings]  # all the phrases
+Y = [saying[1] for saying in sayings]  # all the labels
+
+#taking the tool from the toolbox, creating an instance and has not been used yet
+vectorizer = CountVectorizer()
+#fit → the vectorizer looks at ALL your phrases and learns the vocabulary 
+# transform → converts those phrases into numbers
+X_vectorized = vectorizer.fit_transform(X)
+#taking the tool into the code and hasnt been used yet
+ml_model = MultinomialNB()
+#X_vectorized → the numbers representing phrases
+# Y → the correct labels
+ml_model.fit(X_vectorized, Y)
+
+#creating a new function that reads the ML code and input it into the actual code
+def classify(bot):
+    fitting = vectorizer.transform([bot]) 
+    storage = ml_model.predict(fitting)
+    return storage
+
+def response(intent,user_input):
+    spacers()
+    groq_reply = ask_groq(user_input, intent)
+    print(groq_reply)
+    spacers()
+
+while True:
+
+    #Chatbot asking you what you need
+    bot = input(" What can I help you with?")
+    
+    if bot.lower() in ["quit", "exit"]:
+        spacers()
+        print("Exiting code.")
+        break
+    #calling the classify function and setting the parameter to "bot", so it reads your input
+    intent = classify(bot)
+    response(intent, bot)
